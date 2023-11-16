11e800ba59790cc330af5c34b081819c8af53192
Corey Fulop <corey.fulop@gmail.com>
2023-11-16 17:34:29 +0000

    Refactor code

--- src/index.js
+++ src/index.js
@@ -7,100 +7,94 @@ function todoFactory(title) {
 }
 
 const projects = [];
+let currentProjectArrayLocation;
 
-/* Create new project DOM manipulation starts */
+function displayProjectsInSidebar(sidebar, projectArray) {
+    sidebar.textContent = "";
+    for (let project of projectArray) {
+        let projectTab = document.createElement("div");
+        projectTab.textContent = project.title;
+        projectTab.setAttribute("data-project-array-location", projectArray.indexOf(project));
+        projectTab.addEventListener("click", (event) => {
+            currentProjectArrayLocation = event.target.getAttribute("data-project-array-location");
+        });
+        projectTab.addEventListener("click", (event) => {
+            todoContainer.textContent = "";
+            for (let todo of projectArray[currentProjectArrayLocation].todos) {
+                let singleTodoContainer = document.createElement("div");
+                singleTodoContainer.textContent = todo.title;
+                todoContainer.appendChild(singleTodoContainer);
+            };
+        });
+        projectTab.addEventListener("click", () => {
+            const newToDoBtn = document.createElement("button");
+            newToDoBtn.setAttribute("data-project-array-location", projectArray.indexOf(project));
+            newToDoBtn.textContent = "New ToDo";
+            newToDoBtn.addEventListener("click", () => {
+                newToDoDialog.showModal();
+                document.getElementById("new-todo-title").value = ""; // Clear modal of previous inputs
+            });
+            todoContainer.appendChild(newToDoBtn);
+        });
+        sidebar.appendChild(projectTab);
+    }
+}
+
+const projectTabContainer = document.querySelector(".project-tab-container");
+const todoContainer = document.querySelector(".todo-container");
+
+const newToDoDialog = document.querySelector("#newToDoDialog");
+const createToDoBtn = newToDoDialog.querySelector("#createToDoButton");
+const cancelToDoBtn = newToDoDialog.querySelector("#cancelToDoButton");
 
 const newProjectButton = document.querySelector(".new-project");
 const newProjectDialog = document.querySelector("#newProjectDialog");
 const createProjectBtn = newProjectDialog.querySelector("#createProjectButton");
 const cancelProjectBtn = newProjectDialog.querySelector("#cancelProjectButton");
 
+createToDoBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Do not submit form
+    let newToDoTitle = document.getElementById("new-todo-title").value;
+    let newToDo = todoFactory(newToDoTitle)
+    projects[currentProjectArrayLocation].todos.push(newToDo);
+    newToDoDialog.close();
+    // Mostly duplicated from display projects in sidebar function
+    todoContainer.textContent = "";
+    for (let todo of projects[currentProjectArrayLocation].todos) {
+        let singleTodoContainer = document.createElement("div");
+        singleTodoContainer.textContent = todo.title;
+        todoContainer.appendChild(singleTodoContainer);
+    };
+    const newToDoBtn = document.createElement("button");
+    newToDoBtn.setAttribute("data-project-array-location", currentProjectArrayLocation);
+    newToDoBtn.textContent = "New ToDo";
+    newToDoBtn.addEventListener("click", () => {
+        newToDoDialog.showModal();
+        document.getElementById("new-todo-title").value = ""; // Clear modal of previous inputs
+    });
+    todoContainer.appendChild(newToDoBtn);
+    });
+
+cancelToDoBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Do not submit form
+    newToDoDialog.close();
+    });
+
 newProjectButton.addEventListener("click", () => {
     newProjectDialog.showModal();
-    document.getElementById("new-project-title").value = "";
-});
+    document.getElementById("new-project-title").value = ""; // Clear modal of previous inputs
+    });
 
 createProjectBtn.addEventListener("click", (event) => {
     event.preventDefault(); // Do not submit form
     let newProjectTitle = document.getElementById("new-project-title").value;
     let newProject = projectFactory(newProjectTitle);
     projects.push(newProject);
+    displayProjectsInSidebar(projectTabContainer, projects);
     newProjectDialog.close();
 });
 
 cancelProjectBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // do not submit form
+    event.preventDefault(); // Do not submit form
     newProjectDialog.close();
 });
-
-/* Create new project DOM manipulation ends */
-
-/* Dislay projects in sidebar DOM manipulation starts */
-/* Display contents of project in main area starts */
-
-const projectTabContainer = document.querySelector(".project-tab-container");
-const todoContainer = document.querySelector(".todo-container");
-
-const newToDoDialog = document.querySelector("#newToDoDialog");
-
-const createToDoBtn = newToDoDialog.querySelector("#createToDoButton");
-const cancelToDoBtn = newToDoDialog.querySelector("#cancelToDoButton");
-
-createProjectBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // Do not submit form
-    projectTabContainer.textContent = "";
-    for (let project of projects) {
-        let projectTab = document.createElement("div");
-        projectTab.textContent = project.title;
-        // Event listener below clears the main display, 
-        // 
-        // creates a button 
-        // to add new todos to each project
-        projectTab.addEventListener("click", () => {
-            todoContainer.textContent = "";
-            // display all todos
-            //  will go here
-            const newToDoBtn = document.createElement("button");
-            newToDoBtn.textContent = "New ToDo";
-            // Get position of current project in projects
-            const projectNum = projects.indexOf(project);
-            // Add position as an attribute to button
-            newToDoBtn.setAttribute("data-project-number", projectNum);
-            // Add event listener to button to create new todo
-            // and add todo to corresponding project
-            newToDoBtn.addEventListener("click", (event) => {
-                let projectLoc = event.target.getAttribute("data-project-number");
-                // Display the new ToDo modal
-                newToDoDialog.showModal();
-                // create event listener
-                // get new todo information in event of create
-                createToDoBtn.addEventListener("click", (event) => {
-                    event.preventDefault(); // Do not submit form
-                    let newToDoTitle = document.getElementById("new-todo-title").value;
-                    let newToDo = todoFactory(newToDoTitle)
-                    projects[projectLoc].todos.push(newToDo);
-                    let newToDoTitleField = document.getElementById("new-todo-title");
-                    newToDoDialog.close();
-                    newToDoTitleField.value = "";
-                    console.log(projects);
-                });
-                // create an event listener
-                // cancel new todo in case of pushing cancel
-                cancelToDoBtn.addEventListener("click", (event) => {
-                    event.preventDefault(); // do not submit form
-                    let newToDoTitleField = document.getElementById("new-todo-title");
-                    newToDoDialog.close();
-                    newToDoTitleField.value = "";
-                    console.log(projects);
-                });
-                
-            });
-            todoContainer.appendChild(newToDoBtn);
-        });
-        projectTabContainer.appendChild(projectTab);
-    }
-});
-
-/* Display projects in sidebar DOM manipulation ends */
-/* Display contents of projects in main area ends */
-
